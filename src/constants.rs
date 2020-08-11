421927a614a05865043dd35ee67a2399839eff3a
callumfairbairn <callum.fairbairn@thoughtworks.com>
2020-08-11 10:32:50 +0100

    Add instruction to tell how many tiles per edge there will be given TILE_RES, ZOOM and WINDOW_RES

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,6 @@
 use crate::tile::IPoint2;
 
+// Number of tiles per edge = WINDOW_RES / (TILE_RES * ZOOM))
 pub(crate) const TILE_RES: f32 = 32.0;
 pub(crate) const ZOOM: f32 = 2.0; // This must be a power of 2
 pub(crate) const WINDOW_RES: f32 = 768.0; // This must be a multiple of TILE_RES
